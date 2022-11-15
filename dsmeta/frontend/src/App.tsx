0ffbf6e2cc69942120d0a8a67f43518ce462f327
Alexandre Oliveira <alexandre.oliv.junior@gmail.com>
2022-11-15 10:43:18 -0300

    aula 1

---
+++ dsmeta/frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
+function App() {
+  return(
+    <>
+    <Header></Header>
+    <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+        <SalesCard></SalesCard>
+        </div>
+      </section>
+    </main>
+    </>
+  )
+}
+
+export default App
